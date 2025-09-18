d9a3a6911f932ad79ecadba889f1a388b7952a3e
Manas Doshi <98732831+moodynooby@users.noreply.github.com>
2025-09-18 17:17:24 +0530

    feat: initialize modern portfolio with Vite and React setup
    - Added ESLint configuration for code linting and best practices.
    - Created index.html as the main entry point for the application.
    - Set up package.json with necessary dependencies and scripts for development, building, and linting.
    - Included Vite logo SVG for branding in the application.
    - Developed App component with basic counter functionality and logo display.
    - Implemented Sidebar component for navigation with a drawer toggle.
    - Created LandingPage component to serve as the main page layout.
    - Added React logo SVG for branding in the application.
    - Established global styles in index.css using Tailwind CSS and DaisyUI.
    - Configured main.jsx to render the application in strict mode.
    - Set up Vite configuration for React and Tailwind CSS integration.

---
+++ src/LandingPage.jsx
@@ -0,0 +1,13 @@
+import Sidebar from './Components/Sidebar'
+
+function LP() {
+
+  return (
+    <>
+    <Sidebar drawer={"Menu"} active=""/>
+      <h1>Manas Doshi</h1>
+    </>
+  )
+}
+
+export default LP
